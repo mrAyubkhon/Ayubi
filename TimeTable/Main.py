c3c0b59fd1820512bbf38a6875543482dde4dbd7
mrAyubkhon <146121785+mrAyubkhon@users.noreply.github.com>
2024-09-17 20:59:31 +0500

    Update Main.py

--- TimeTable/Main.py
+++ TimeTable/Main.py
@@ -1,13 +1,14 @@
-from email.mime import message
-from pyexpat.errors import messages
-
 import telebot  # Импортируем библиотеку для работы с Telegram Bot API
-import webbrowser  # Импортируем библиотеку для открытия веб-страниц
+import os  # Для работы с переменными окружения
+from flask import Flask, request  # Импортируем Flask для работы с вебхуками
 from telebot import types
 
 # Создаём объект бота с использованием токена
-bot = telebot.TeleBot('7365761529:AAFYeDS9e4uWiTk9xBxDebdfs0XEX1BOcPg')
+TOKEN = os.getenv("TELEGRAM_TOKEN", '7365761529:AAFYeDS9e4uWiTk9xBxDebdfs0XEX1BOcPg')  # Подключаем токен из переменных окружения для безопасности
+bot = telebot.TeleBot(TOKEN)
 
+# Создаем Flask-приложение
+app = Flask(__name__)
 
 # Обработчик команды '/start'
 @bot.message_handler(commands=['start'])
@@ -28,17 +29,17 @@ def start(message):
     btn10 = types.KeyboardButton('Ping Pong')
     btn11 = types.KeyboardButton('Time Table website')
 
-
     # Добавляем кнопки в клавиатуру
-    markup.add(btn1, btn2, btn3, btn4, btn5, btn6, btn7 , btn8 , btn9 , btn10 , btn11 )
+    markup.add(btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn10, btn11)
 
     # Отправляем сообщение с клавиатурой
     bot.send_message(
         message.chat.id,
         f'Hi, how can I help you, miss/mister {message.from_user.first_name} {message.from_user.last_name}. Choose a day of the <b>Week</b>,<b>Gym</b> or <b>Website</b>',
-        parse_mode='HTML',reply_markup=markup
+        parse_mode='HTML', reply_markup=markup
     )
 
+# Остальные обработчики, как и раньше
 @bot.message_handler(func=lambda message: message.text == 'CS4-23')
 def cs423(message):
     photo = open('CS4-23.png', 'rb')  # Укажите путь к вашему фото
@@ -51,15 +52,10 @@ def cs523(message):
     bot.send_photo(message.chat.id, photo)
     photo.close()
 
-
-
-# Обработчик кнопки "Time Table website"
 @bot.message_handler(func=lambda message: message.text == 'Time Table website')
 def site(message):
     bot.send_message(message.chat.id, "Here is the link to the schedule: https://cau.edupage.org/timetable/")
 
-
-# Обработчики для дней недели и Gym
 @bot.message_handler(func=lambda message: message.text == 'Monday')
 def monday(message):
     bot.send_message(
@@ -69,7 +65,6 @@ def monday(message):
         f'13:30-14:50 <b>Software Engineering</b> in <b>104 room</b>',
         parse_mode='HTML'
     )
-
 
 @bot.message_handler(func=lambda message: message.text == 'Tuesday')
 def tuesday(message):
@@ -81,7 +76,6 @@ def tuesday(message):
         parse_mode='HTML'
     )
 
-
 @bot.message_handler(func=lambda message: message.text == 'Wednesday')
 def wednesday(message):
     bot.send_message(
@@ -90,7 +84,6 @@ def wednesday(message):
         f'11:00-12:20 <b>Software Engineering</b> in <b>102 room</b>',
         parse_mode='HTML'
     )
-
 
 @bot.message_handler(func=lambda message: message.text == 'Thursday')
 def thursday(message):
@@ -101,7 +94,6 @@ def thursday(message):
         parse_mode='HTML'
     )
 
-
 @bot.message_handler(func=lambda message: message.text == 'Friday')
 def friday(message):
     bot.send_message(
@@ -111,7 +103,6 @@ def friday(message):
         parse_mode='HTML'
     )
 
-
 @bot.message_handler(func=lambda message: message.text == 'Gym')
 def gym(message):
     bot.send_message(
@@ -119,6 +110,7 @@ def gym(message):
         f'<b>9:00-13:00</b>, <b>14:00-18:00</b> you can enter\n'
         f'Monday - Friday', parse_mode='HTML'
     )
+
 @bot.message_handler(func=lambda message: message.text == 'Swimming Pool')
 def swimming(message):
     bot.send_message(
@@ -128,6 +120,7 @@ def swimming(message):
         f'<b>Warning: </b> you can enter if you men in <b>Monday ,Wednesday and Friday</b> in <b>14:00 - 18:00</b> \n'
         f'In <b>Tuesday</b> and <b>Thursday</b>  you can enter in <b>9:00 - 12:50 </b>', parse_mode='HTML'
     )
+
 @bot.message_handler(func=lambda message: message.text == 'Ping Pong')
 def ping_pong(message):
     bot.send_message(
@@ -135,7 +128,19 @@ def ping_pong(message):
         f'<b>9:00-13:00</b>, <b>14:00-18:00</b> you can enter' , parse_mode='HTML'
     )
 
+# Flask route для обработки вебхуков от Telegram
+@app.route(f"/{TOKEN}", methods=['POST'])
+def receive_update():
+    json_str = request.get_data().decode('UTF-8')
+    update = telebot.types.Update.de_json(json_str)
+    bot.process_new_updates([update])
+    return "!", 200
 
+# Главная страница для проверки работы сервера
+@app.route("/", methods=["GET"])
+def index():
+    return "Telegram bot is running!", 200
 
-# Запускаем бота для постоянного прослушивания сообщений
-bot.polling(none_stop=True)
+# Запуск приложения Flask (для локальной разработки и Vercel)
+if __name__ == "__main__":
+    app.run(debug=True)
